7dde9ab82b215aefd45e851590f94ca0b0ed6e09
sohseyedi <sohseyedi@gmail.com>
2025-02-24 11:08:29 +0330

    style: lint app

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 import { Toaster } from "react-hot-toast";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { Provider } from "react-redux";
-import store from '@/store/store.ts';
+import store from "@/store/store.ts";
 
 const queryClient = new QueryClient();
 
