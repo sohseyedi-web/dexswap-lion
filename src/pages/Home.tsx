7c7a4df25248ce874e038b13b5907c05d36158fc
sohseyedi <sohseyedi@gmail.com>
2025-02-24 17:58:00 +0330

    feat: create Home component to integrate SwapBox

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import SwapBox from "@/features/home/SwapBox";
+
+const Home = () => {
+  return (
+    <section className="flex items-center justify-center h-screen">
+      <SwapBox />
+    </section>
+  );
+};
+
+export default Home;
