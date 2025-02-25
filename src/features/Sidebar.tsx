460b65b6fb1fe02e7967a2ac9a8f0b541ae1f532
sohseyedi <sohseyedi@gmail.com>
2025-02-25 13:41:04 +0330

    feat: add responsive sidebar component

---
+++ src/features/Sidebar.tsx
@@ -0,0 +1,37 @@
+import { useResponsiveStore } from "@/store/useResponsive";
+import Back from "@/ui/Back";
+import { SiRobotframework } from "react-icons/si";
+import { Link } from "react-router-dom";
+
+const Sidebar = () => {
+  const { active } = useResponsiveStore();
+
+  return (
+    <>
+      <Back />
+      <aside
+        className={`${
+          active ? "right-0 top-0" : "-right-64 top-0"
+        } fixed z-40 w-[240px] bg-[#212121] h-screen border-l-2 border-zinc-800 py-6 px-4 space-y-3 transition-all duration-300`}
+      >
+        <div className="flex items-center gap-x-3">
+          <SiRobotframework size={38} className="text-[#2cb67d]" />
+          <h6 className="text-2xl font-semibold">سو سواپ</h6>
+        </div>
+        <ul className="flex flex-col space-y-7 pt-7">
+          <Link to={"/"} className="text-lg font-semibold text-[#2cb67d]">
+            صفحه اصلی
+          </Link>
+          <Link to={"/"} className="text-lg font-semibold text-zinc-300">
+            توکن ها
+          </Link>
+          <Link to={"/"} className="text-lg font-semibold text-zinc-300">
+            ابزار ها
+          </Link>
+        </ul>
+      </aside>
+    </>
+  );
+};
+
+export default Sidebar;
