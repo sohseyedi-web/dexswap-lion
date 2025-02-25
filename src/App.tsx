990c234739d2a04e1e479d0935e3ab6ff56bf81e
sohseyedi <sohseyedi@gmail.com>
2025-02-25 10:21:47 +0330

    chore: add Navbar and Support components into App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Navbar from "./features/Navbar";
+import SupportIcon from "./ui/SupportIcon";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+      <SupportIcon/>
     </BrowserRouter>
   );
 }
