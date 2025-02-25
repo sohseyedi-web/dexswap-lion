8b41090469e7db079fcb852e1897e18d5e8cda3f
sohseyedi <sohseyedi@gmail.com>
2025-02-25 13:40:34 +0330

    feat: add responsive media update on window resize

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,24 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./features/Navbar";
 import SupportIcon from "./ui/SupportIcon";
+import { useResponsiveStore } from "./store/useResponsive";
+import { useEffect } from "react";
 
 function App() {
+  const { updateMedia } = useResponsiveStore();
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  }, [updateMedia]);
+
   return (
     <BrowserRouter>
-      <Navbar/>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-      <SupportIcon/>
+      <SupportIcon />
     </BrowserRouter>
   );
 }
