987a9a9ca4acf886ee727b8119b01d6c115ee548
sohseyedi <sohseyedi@gmail.com>
2025-02-26 18:24:08 +0330

    chore:direction modal

--- src/ui/ModalWrapper.tsx
+++ src/ui/ModalWrapper.tsx
@@ -1,5 +1,6 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { ReactNode } from "react";
+import { RiCloseLine } from "react-icons/ri";
 
 type ModalPropTypes = {
   isOpen: boolean;
@@ -12,7 +13,10 @@ const ModalWrapper = ({ isOpen, onClose, children, title }: ModalPropTypes) => {
   return (
     <AnimatePresence>
       {isOpen && (
-        <div className="fixed inset-0 flex items-center justify-center z-50">
+        <div
+          className="fixed inset-0 flex items-center justify-center z-50"
+          dir="rtl"
+        >
           <motion.div
             className="fixed inset-0 bg-black bg-opacity-50 backdrop-blur-sm"
             initial={{ opacity: 0 }}
@@ -22,17 +26,22 @@ const ModalWrapper = ({ isOpen, onClose, children, title }: ModalPropTypes) => {
           />
 
           <motion.div
-            className="relative bg-[#212121] rounded-xl p-4 shadow-lg sm:w-full md:max-w-md w-[90%]"
+            className="relative bg-[#212121] rounded-xl p-4 shadow-lg lg:w-[33%] md:w-[60%] w-[90%]"
             initial={{ opacity: 0, scale: 0.8 }}
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0.8 }}
             transition={{ type: "spring", stiffness: 300, damping: 20 }}
           >
-            <div className="space-y-4 md:p-3">
+            <div className="space-y-4 p-2">
               <div className="flex items-center justify-between">
                 <h4 className="text-xl font-semibold text-[#2cb67d]">
                   {title}
                 </h4>
+                <RiCloseLine
+                  size={28}
+                  className="cursor-pointer"
+                  onClick={onClose}
+                />
               </div>
               <div className="my-2">{children}</div>
             </div>
