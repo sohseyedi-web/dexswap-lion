3a03cc76dd058096973fdb50f67d7f7b0fdf39f6
sohseyedi <sohseyedi@gmail.com>
2025-02-26 18:36:14 +0330

    chore: integrate modal for token list display

--- src/features/home/FieldToken.tsx
+++ src/features/home/FieldToken.tsx
@@ -1,7 +1,12 @@
 import { FieldTokenTypes } from "@/types";
+import ModalWrapper from "@/ui/ModalWrapper";
 import { RiArrowDownSLine } from "react-icons/ri";
+import TokenListBox from "./TokenListBox";
+import { useState } from "react";
 
 const FieldToken = ({ title }: FieldTokenTypes) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <section className="p-3 rounded-2xl border border-zinc-800" dir="rtl">
       <div className="flex items-center gap-x-2">
@@ -11,7 +16,7 @@ const FieldToken = ({ title }: FieldTokenTypes) => {
         <span className="font-bold"> : USDT</span>
       </div>
       <div className="flex md:items-center md:flex-row flex-col justify-center gap-x-3 my-2">
-        <div className="md:h-[50px] h-[40px] md:w-[30%] w-[60%] bg-black rounded-2xl md:mb-0 mb-3 cursor-pointer px-2 flex items-center justify-between">
+        <div onClick={() => setIsOpen(true)} className="md:h-[50px] h-[40px] lg:w-[35%] w-[60%] bg-black rounded-2xl md:mb-0 mb-3 cursor-pointer px-2 flex items-center justify-between">
           {/* <div className="flex items-center gap-x-3 mr-1">
             <span>s</span>
             <span>تتر</span>
@@ -27,9 +32,20 @@ const FieldToken = ({ title }: FieldTokenTypes) => {
       </div>
       <p className="text-sm text-zinc-400">
         قیمت :{" "}
-        <span className="font-semibold text-[#2cb67d] md:ml-2">10.2 $ </span> معادل{" "}
-        <span className="font-semibold text-[#2cb67d] md:mr-2"> 90/000 تومان</span>
+        <span className="font-semibold text-[#2cb67d] md:ml-2">10.2 $ </span>{" "}
+        معادل{" "}
+        <span className="font-semibold text-[#2cb67d] md:mr-2">
+          {" "}
+          90/000 تومان
+        </span>
       </p>
+      <ModalWrapper
+        title="لیست ارز ها"
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      >
+        <TokenListBox />
+      </ModalWrapper>
     </section>
   );
 };
