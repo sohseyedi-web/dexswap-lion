3de07fcbe45ff14dfd48c74c3ddf5da6083979fc
sohseyedi <sohseyedi@gmail.com>
2025-02-26 21:46:48 +0330

    feat: add TokenInterface for token data structure

---
+++ src/types/TokenTypes.ts
@@ -0,0 +1,11 @@
+export interface TokenInterFace {
+  _id: string;
+  symbol: string;
+  name: string;
+  img: string;
+  address: string;
+  decimals: number;
+  chain: string;
+  rank: number;
+  __v: number;
+}
