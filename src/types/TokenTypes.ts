edc506fd0d2939087e64b04ba4db0ec56a938b69
sohseyedi <sohseyedi@gmail.com>
2025-03-02 17:43:52 +0330

    chore: add TokenPriceInterFace

--- src/types/TokenTypes.ts
+++ src/types/TokenTypes.ts
@@ -9,3 +9,17 @@ export interface TokenInterFace {
   rank: number;
   __v: number;
 }
+
+export interface TokenPriceInterFace {
+  usdPrice: number;
+  usdPriceFormatted: string;
+  nativePrice: {
+    value: string;
+    decimals: number;
+    name: string;
+    symbol: string;
+    address: string;
+  };
+  dayPercentChange: string;
+  tokenAddress : string
+}
