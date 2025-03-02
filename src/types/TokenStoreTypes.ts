76fc962f5de34d450a6151878bb891d88af74a48
sohseyedi <sohseyedi@gmail.com>
2025-03-02 17:44:02 +0330

    chore: extend TokenState to include token prices and update methods

--- src/types/TokenStoreTypes.ts
+++ src/types/TokenStoreTypes.ts
@@ -1,11 +1,14 @@
-import { TokenInterFace } from "./TokenTypes";
+import { TokenInterFace, TokenPriceInterFace } from "./TokenTypes";
 
 export type TokenState = {
   activeToken: number;
   token1: TokenInterFace;
   token2: TokenInterFace;
+  token1Price: TokenPriceInterFace;
+  token2Price: TokenPriceInterFace;
   pinTokens: TokenInterFace[];
   updateItemData: (data: TokenInterFace) => void;
+  setTokenPrice: (data: TokenPriceInterFace) => void;
   setActiveToken: (item: number) => void;
   addPinToken: (token: TokenInterFace) => void;
   removePinToken: (token: TokenInterFace) => void;
