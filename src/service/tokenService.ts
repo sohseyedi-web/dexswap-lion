6f98308af79297fcc1b0a0d697a78856de755ea6
sohseyedi <sohseyedi@gmail.com>
2025-03-03 10:37:56 +0330

    chore: add chain parameter to function

--- src/service/tokenService.ts
+++ src/service/tokenService.ts
@@ -1,8 +1,8 @@
 import http from "./http";
 
-export async function getPriceToken(token: string) {
+export async function getPriceToken(token: string, chain: string) {
   try {
-    const { data } = await http.get(`/erc20/${token}/price?chain=eth`);
+    const { data } = await http.get(`/erc20/${token}/price?chain=${chain}`);
     return data;
   } catch (error) {
     console.log(error);
