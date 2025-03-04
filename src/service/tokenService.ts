f0c4c6621ddc4aa527898de3b1971fa66320c0ff
sohseyedi <sohseyedi@gmail.com>
2025-03-04 11:00:12 +0330

    chore: create getTOKENIRT function

--- src/service/tokenService.ts
+++ src/service/tokenService.ts
@@ -1,8 +1,20 @@
+import axios from "axios";
 import http from "./http";
 
 export async function getPriceToken(token: string, chain: string) {
   try {
     const { data } = await http.get(`/erc20/${token}/price?chain=${chain}`);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getTOKENIRT(token: string) {
+  try {
+    const { data } = await axios.get(
+      `${import.meta.env.VITE_API_IRT}/orderbook/${token}`
+    );
     return data;
   } catch (error) {
     console.log(error);
