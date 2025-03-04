ab548630dbf8c4312f660267e03990c0962d8e6e
sohseyedi <sohseyedi@gmail.com>
2025-03-04 11:00:30 +0330

    chore: fetch and update token price using getTOKENIRT function

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,28 @@ import Navbar from "./features/Navbar";
 import { useResponsiveStore } from "./store/useResponsive";
 import { useEffect } from "react";
 import Sidebar from "./features/Sidebar";
+import { useTokenStore } from "./store/useTokenStore";
+import { getTOKENIRT } from "./service/tokenService";
 
 function App() {
   const { updateMedia } = useResponsiveStore();
+  const { addTomanPrice } = useTokenStore();
 
   useEffect(() => {
     window.addEventListener("resize", updateMedia);
     return () => window.removeEventListener("resize", updateMedia);
   }, [updateMedia]);
+
+  const fetchTokenPrice = async () => {
+    const toman = await getTOKENIRT("USDTIRT");
+    addTomanPrice(toman?.lastTradePrice);
+  };
+
+  useEffect(() => {
+    fetchTokenPrice();
+    const interval = setInterval(fetchTokenPrice, 60 * 60 * 1000);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <BrowserRouter>
