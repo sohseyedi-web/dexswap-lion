c94097efd05b022b3ec42fec02eb334d85f5df31
sohseyedi <sohseyedi@gmail.com>
2025-03-06 18:59:10 +0330

    chore: enhance Tether page with exchange selection and improved loading state

--- src/pages/Tether.tsx
+++ src/pages/Tether.tsx
@@ -1,27 +1,18 @@
 import TetherBox from "@/features/tetherprice/TetherBox";
+import TetherExchange from "@/features/tetherprice/TetherExchange";
 import { getUsdtPrice } from "@/service/tokenService";
+import { TetherData } from "@/types";
+import Loading from "@/ui/Loading";
 import { toPersianNumbersWithComma } from "@/utils/toPersianNumber";
 import { useEffect, useState } from "react";
-
-interface TetherData {
-  price: number;
-  diff24d: string;
-  diff7d: string;
-  diff30d: string;
-  last24h: number;
-  last24hMin: number;
-  last24hMax: number;
-  last7d: number;
-  last7dMin: number;
-  last7dMax: number;
-  last30d: number;
-  last30dMin: number;
-  last30dMax: number;
-}
 
 const Tether = () => {
   const [item, setItem] = useState<TetherData | null>(null);
   const [loading, setLoading] = useState(false);
+  const [selectedExchange, setSelectedExchange] = useState({
+    name: "تترلند",
+    id: 1,
+  });
 
   const fetchTether = async () => {
     try {
@@ -43,39 +34,51 @@ const Tether = () => {
 
   return (
     <section className="max-w-7xl mx-auto mt-5 lg:px-0 px-6 pb-6" dir="rtl">
-      <header className="mb-5 flex items-center gap-x-3">
+      <header className="mb-5 flex items-center justify-between ">
         <h4 className="lg:text-xl text-lg font-semibold text-[#2cb67d]">
-          قیمت آنلاین تتر :
+          قیمت آنلاین تتر
         </h4>
+        <TetherExchange
+          select={selectedExchange}
+          setSelect={setSelectedExchange}
+        />
+      </header>
+      <div className="flex items-center gap-3 md:my-3 my-6">
+        <h6 className="font-semibold text-[#2cb67d] md:text-lg">
+          صرافی {selectedExchange.name} :
+        </h6>
         {loading ? (
-          <span>در حال بارگذاری...</span>
+          <Loading className="bg-[#2cb67d]" />
         ) : (
-          <span className="lg:text-xl">
+          <span className="md:text-xl text-sm">
             {toPersianNumbersWithComma(String(item?.price))} تومان
           </span>
         )}
-      </header>
-      <h6 className="font-semibold text-[#2cb67d] text-lg mb-3">صرافی تترلند</h6>
-      <div className="grid md:grid-cols-3 grid-cols-1 gap-4">
-        <TetherBox
-          day={"1"}
-          diffDay={item?.diff24d ?? ""}
-          lastDayMax={item?.last24hMax ?? 0}
-          lastDayMin={item?.last24hMin ?? 0}
-        />
-        <TetherBox
-          day={"7"}
-          diffDay={item?.diff7d ?? ""}
-          lastDayMax={item?.last7dMax ?? 0}
-          lastDayMin={item?.last7dMin ?? 0}
-        />
-        <TetherBox
-          day={"30"}
-          diffDay={item?.diff30d ?? ""}
-          lastDayMax={item?.last30dMax ?? 0}
-          lastDayMin={item?.last30dMin ?? 0}
-        />
       </div>
+      {loading ? (
+        <Loading className="bg-[#2cb67d]" />
+      ) : (
+        <div className="grid md:grid-cols-3 grid-cols-1 gap-4">
+          <TetherBox
+            day={"1"}
+            diffDay={item?.diff24d ?? ""}
+            lastDayMax={item?.last24hMax ?? 0}
+            lastDayMin={item?.last24hMin ?? 0}
+          />
+          <TetherBox
+            day={"7"}
+            diffDay={item?.diff7d ?? ""}
+            lastDayMax={item?.last7dMax ?? 0}
+            lastDayMin={item?.last7dMin ?? 0}
+          />
+          <TetherBox
+            day={"30"}
+            diffDay={item?.diff30d ?? ""}
+            lastDayMax={item?.last30dMax ?? 0}
+            lastDayMin={item?.last30dMin ?? 0}
+          />
+        </div>
+      )}
     </section>
   );
 };
