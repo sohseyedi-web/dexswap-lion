fc1e5011d19fb0c39cc5f0b1bb3002ac1e3d160b
sohseyedi <sohseyedi@gmail.com>
2025-03-06 18:58:26 +0330

    feat: add TetherData interface for Tether price tracking

---
+++ src/types/TetherTypes.ts
@@ -0,0 +1,15 @@
+export interface TetherData {
+  price: number;
+  diff24d: string;
+  diff7d: string;
+  diff30d: string;
+  last24h: number;
+  last24hMin: number;
+  last24hMax: number;
+  last7d: number;
+  last7dMin: number;
+  last7dMax: number;
+  last30d: number;
+  last30dMin: number;
+  last30dMax: number;
+}
