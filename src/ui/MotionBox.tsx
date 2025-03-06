b2c8d1df3f7ee0bcf57b46e4b32a1aaf24de255b
sohseyedi <sohseyedi@gmail.com>
2025-03-06 10:25:43 +0330

    feat: create MotionBox component with framer-motion animation

---
+++ src/ui/MotionBox.tsx
@@ -0,0 +1,24 @@
+import { motion } from "framer-motion";
+import { useMemo } from "react";
+
+const MotionBox = ({ children }: { children: React.ReactNode }) => {
+  const motionProps = useMemo(
+    () => ({
+      initial: { opacity: 0, scale: 0.9 },
+      animate: { opacity: 1, scale: 1 },
+      transition: { duration: 0.3 },
+    }),
+    []
+  );
+
+  return (
+    <motion.div
+      {...motionProps}
+      className="lg:w-[33%] md:w-[60%] space-y-4 md:mt-0 mt-10 w-[90%] p-5 border border-zinc-800 rounded-2xl bg-[#212121] hover:shadow-xl transition-all duration-300"
+    >
+      {children}
+    </motion.div>
+  );
+};
+
+export default MotionBox;
