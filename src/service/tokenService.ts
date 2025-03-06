61617a62b18b046addd349960f417064d0ab9d27
sohseyedi <sohseyedi@gmail.com>
2025-03-06 09:52:52 +0330

    chore: add getUsdtPrice method

--- src/service/tokenService.ts
+++ src/service/tokenService.ts
@@ -10,12 +10,23 @@ export async function getPriceToken(token: string, chain: string) {
   }
 }
 
-export async function getTOKENIRT(token: string) {
+// export async function getTOKENIRT(token: string) {
+//   try {
+//     const { data } = await axios.get(
+//       `${import.meta.env.VITE_API_NOBITEX}/orderbook/${token}`
+//     );
+//     return data;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+export async function getUsdtPrice() {
   try {
     const { data } = await axios.get(
-      `${import.meta.env.VITE_API_IRT}/orderbook/${token}`
+      `${import.meta.env.VITE_API_TETHERLAND}/currencies`
     );
-    return data;
+    return data?.data?.currencies;
   } catch (error) {
     console.log(error);
   }
