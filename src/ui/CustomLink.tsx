15ba4486d5b2b8b909af8a1672ff19e361478e13
sohseyedi <sohseyedi@gmail.com>
2025-03-06 11:40:22 +0330

    chore: add close functionality to CustomLink

--- src/ui/CustomLink.tsx
+++ src/ui/CustomLink.tsx
@@ -1,3 +1,4 @@
+import { useResponsiveStore } from "@/store/useResponsive";
 import { NavLink } from "react-router-dom";
 
 type CustomLinkProps = {
@@ -6,14 +7,18 @@ type CustomLinkProps = {
 };
 
 export const Customlink = ({ name, to }: CustomLinkProps) => {
+  const { setActive } = useResponsiveStore();
   const navlinkClass = "transition-all duration-300 text-lg font-semibold";
 
   return (
     <li className="list-none">
       <NavLink
+        onClick={() => setActive(false)}
         to={to}
         className={({ isActive }) =>
-          isActive ? `${navlinkClass} text-[#2cb67d]` : `${navlinkClass} hover:text-[#27cb67]`
+          isActive
+            ? `${navlinkClass} text-[#2cb67d]`
+            : `${navlinkClass} hover:text-[#27cb67]`
         }
       >
         {name}
