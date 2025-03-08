398fbe91e7de3d4bc5bc50c63c1cfccd314c74a2
sohseyedi <sohseyedi@gmail.com>
2025-03-08 16:31:49 +0330

    chore:add animation for tabel

--- src/pages/Tokens.tsx
+++ src/pages/Tokens.tsx
@@ -1,12 +1,18 @@
 import TokensTable from '@/features/tokenList/TokenTable'
 import TokenTableHeader from '@/features/tokenList/TokenTableHeader'
+import { motion } from 'framer-motion'
 
 const Tokens = () => {
   return (
-    <section className="max-w-7xl mx-auto lg:px-0 px-6 pb-6">
+    <motion.section
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.5 }}
+      className="max-w-7xl mx-auto lg:px-0 px-6 pb-6"
+    >
       <TokenTableHeader />
       <TokensTable />
-    </section>
+    </motion.section>
   );
 }
 
