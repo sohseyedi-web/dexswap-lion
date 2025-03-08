b8b9fa1bf54239cae3c6fa936712a29ff0654e51
sohseyedi <sohseyedi@gmail.com>
2025-03-08 12:10:08 +0330

    feat: add token table headers constant

---
+++ src/constant/tokenHeadLists.ts
@@ -0,0 +1,30 @@
+export const tokenTHeads = [
+  {
+    id: 1,
+    label: "#",
+  },
+  {
+    id: 2,
+    label: "نام",
+  },
+  {
+    id: 3,
+    label: "نماد",
+  },
+  {
+    id: 4,
+    label: "قیمت",
+  },
+  {
+    id: 5,
+    label: "کمترین قیمت ۲۴h",
+  },
+  {
+    id: 6,
+    label: "بیشترین قیمت ۲۴h",
+  },
+  {
+    id: 7,
+    label: "درصد تغییرات",
+  },
+];
