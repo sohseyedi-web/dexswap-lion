c10366f00ae99d49699c86efeca872c624feee95
sohseyedi <sohseyedi@gmail.com>
2025-03-09 17:25:34 +0330

    chore: add pagination support to table store

--- src/store/useTableStore.ts
+++ src/store/useTableStore.ts
@@ -3,13 +3,21 @@ import { create } from "zustand";
 type TableState = {
   search: string;
   active: boolean;
+  page: number;
+  hasMore: boolean;
   setActive: (active: boolean) => void;
   setSearch: (search: string) => void;
+  setPage: (page: number) => void;
+  setHasMore: (hasMore: boolean) => void;
 };
 
 export const useTableStore = create<TableState>((set) => ({
   search: "",
   active: false,
+  page: 1,
+  hasMore: true,
   setActive: (active) => set({ active }),
   setSearch: (search) => set({ search }),
+  setPage: (page) => set({ page }),
+  setHasMore: (hasMore) => set({ hasMore }),
 }));
