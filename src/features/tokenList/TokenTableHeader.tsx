d6cee508504af8ce95856f723b17df9ce1096045
sohseyedi <sohseyedi@gmail.com>
2025-03-09 18:44:55 +0330

    chore:remove icon

--- src/features/tokenList/TokenTableHeader.tsx
+++ src/features/tokenList/TokenTableHeader.tsx
@@ -1,5 +1,5 @@
 import { useTableStore } from "@/store/useTableStore";
-import { RiFilter3Fill, RiSearchLine } from "react-icons/ri";
+import { RiSearchLine } from "react-icons/ri";
 
 const TokenTableHeader = () => {
   const { setActive, active, search, setSearch } = useTableStore();
@@ -12,23 +12,19 @@ const TokenTableHeader = () => {
           value={search}
           onChange={(e) => setSearch(e.target.value)}
           placeholder="جستجوی ارز ..."
-          className={`w-full pr-12 placeholder:text-zinc-500 outline-none h-[50px] rounded-2xl bg-transparent border border-zinc-700 transition-all duration-300`}
+          className={`w-full pr-12 placeholder:text-zinc-500 outline-none md:h-[50px] h-[45px] rounded-2xl bg-transparent border border-zinc-700 transition-all duration-300`}
         />
         <RiSearchLine
           size={28}
           className="absolute text-emerald-700 top-3 right-2 rotate-90 cursor-pointer"
         />
       </div>
-
-      <div className="flex items-center gap-x-2 text-[#2cb67d]">
-        <span
-          className="cursor-pointer text-sm transition-all duration-200"
-          onClick={() => setActive(!active)}
-        >
-          {active ? "دلار" : "تومان"}
-        </span>
-        <RiFilter3Fill size={28} className="cursor-pointer" />
-      </div>
+      <span
+        className="cursor-pointer text-sm transition-all duration-200"
+        onClick={() => setActive(!active)}
+      >
+        {active ? "دلار" : "تومان"}
+      </span>
     </header>
   );
 };
