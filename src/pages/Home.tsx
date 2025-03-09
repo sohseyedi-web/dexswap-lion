fd2d2c1be34624a9e866a4a84eb0cf3d3fb4991b
sohseyedi <sohseyedi@gmail.com>
2025-03-09 18:35:58 +0330

    style:add padding bottom

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import SwapBox from "@/features/home/SwapBox";
 
 const Home = () => {
   return (
-    <section className="flex items-center justify-center md:h-[80vh] h-full">
+    <section className="flex items-center justify-center md:h-[80vh] h-full md:pb-0 pb-3">
       <SwapBox />
     </section>
   );
