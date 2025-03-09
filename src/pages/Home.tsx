6e9b5b2110ee6fb00de8d67e9afb669ebe543ec0
sohseyedi <sohseyedi@gmail.com>
2025-03-09 18:12:18 +0330

    style: add page section vertical padding

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import SwapBox from "@/features/home/SwapBox";
 
 const Home = () => {
   return (
-    <section className="flex items-center justify-center h-[80vh]">
+    <section className="flex items-center justify-center h-[80vh] py-8">
       <SwapBox />
     </section>
   );
