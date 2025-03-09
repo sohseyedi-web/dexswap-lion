b9b1beeef36749d6ac368b388bb97243f38b6de3
sohseyedi <sohseyedi@gmail.com>
2025-03-09 18:26:23 +0330

    style:change margin & height box

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import SwapBox from "@/features/home/SwapBox";
 
 const Home = () => {
   return (
-    <section className="flex items-center justify-center h-[80vh] py-8">
+    <section className="flex items-center justify-center md:h-[80vh] h-full">
       <SwapBox />
     </section>
   );
--- src/ui/MotionBox.tsx
+++ src/ui/MotionBox.tsx
@@ -15,7 +15,7 @@ const MotionBox = ({ children }: { children: React.ReactNode }) => {
     <motion.div
       dir="rtl"
       {...motionProps}
-      className="lg:w-[33%] md:w-[60%] space-y-4 md:mt-0 mt-10 w-[90%] md:p-5 p-4 border border-zinc-800 rounded-2xl bg-[#212121] hover:shadow-xl transition-all duration-300"
+      className="lg:w-[33%] md:w-[60%] space-y-4 md:mt-0 mt-5 w-[90%] md:p-5 p-4 border border-zinc-800 rounded-2xl bg-[#212121] hover:shadow-xl transition-all duration-300"
     >
       {children}
     </motion.div>
