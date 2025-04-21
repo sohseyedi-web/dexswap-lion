980b5217ff706c2d40dcf0b79893ff96e4f941c5
sohseyedi <sohseyedi@gmail.com>
2025-04-21 13:01:54 +0330

    chore: add wagmi provider for blockchain connectivity

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,36 @@ import "./index.css";
 import "./i18n";
 import { Toaster } from "react-hot-toast";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { createConfig, http } from "wagmi";
+import { WagmiProvider } from "wagmi";
+import { mainnet, polygon, optimism, arbitrum } from "wagmi/chains";
+import { injected, metaMask } from "wagmi/connectors";
 
 const queryClient = new QueryClient();
 
+const config = createConfig({
+  chains: [mainnet, polygon, optimism, arbitrum],
+  connectors: [
+    metaMask(),
+    injected({
+      shimDisconnect: true,
+    }),
+  ],
+  transports: {
+    [mainnet.id]: http(),
+    [polygon.id]: http(),
+    [optimism.id]: http(),
+    [arbitrum.id]: http(),
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <Toaster />
-    </QueryClientProvider>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <Toaster />
+      </QueryClientProvider>
+    </WagmiProvider>
   </React.StrictMode>
 );
